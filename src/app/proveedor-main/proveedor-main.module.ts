37f262ac30036153c04e392385b01443fff8970f
AlejoG10 <alejogonza99@hotmail.com>
2022-04-30 18:10:57 -0500

    Modify proveedor-main module

--- src/app/proveedor-main/proveedor-main.module.ts
+++ src/app/proveedor-main/proveedor-main.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProveedorMainComponent } from './proveedor-main/proveedor-main.component';
 
-import { NavbarModule } from '../navbar/navbar.module';
-
 @NgModule({
-  imports: [
-    CommonModule,
-    NavbarModule,
-  ],
+  imports: [CommonModule],
   declarations: [ProveedorMainComponent],
-  exports: [ProveedorMainComponent]
+  exports: [ProveedorMainComponent],
 })
-export class ProveedorMainModule { }
+export class ProveedorMainModule {}
