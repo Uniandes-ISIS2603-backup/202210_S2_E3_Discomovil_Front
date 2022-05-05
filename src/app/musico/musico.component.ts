45fb159972c8afc30003fe1682773899f786714f
Daniel Reales <da.reales@uniandes.edu.co>
2022-05-04 19:12:30 -0500

    Agregar modulo de musico

---
+++ src/app/musico/musico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-musico',
+  templateUrl: './musico.component.html',
+  styleUrls: ['./musico.component.css']
+})
+export class MusicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/musico/musico.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MusicoComponent } from './musico.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MusicoComponent]
+})
+export class MusicoModule { }
