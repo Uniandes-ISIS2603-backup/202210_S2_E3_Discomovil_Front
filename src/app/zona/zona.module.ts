0dcc0ed574414e711bfbcf238ba54ab5dd8c78ba
aspd-2501 <a.padillad@uniandes.edu.co>
2022-05-04 23:28:52 -0500

    zona module :3

---
+++ src/app/zona/zona.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ZonaListComponent } from './zona-list/zona-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ZonaListComponent]
+})
+export class ZonaModule { }
