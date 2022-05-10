6314fb6ee7fd5b232fe520a81886dd64ab3c3ad1
Majo Cely <m.cely@uniandes.edu.co>
2022-05-10 15:09:17 -0500

    Pruebas list-datail

--- src/app/proveedor/proveedor.service.spec.ts
+++ src/app/proveedor/proveedor.service.spec.ts
@@ -1,16 +1,17 @@
-/* tslint:disable:no-unused-variable */
-
 import { TestBed, async, inject } from '@angular/core/testing';
 import { ProveedorService } from './proveedor.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('Service: Proveedor', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [ProveedorService]
     });
   });
 
-  // it('should ...', inject([ProveedorService], (service: ProveedorService) => {
-  //   expect(service).toBeTruthy();
-  // }));
+  it('should ...', inject([ProveedorService], (service: ProveedorService) => {
+    expect(service).toBeTruthy();
+  }));
 });
+
