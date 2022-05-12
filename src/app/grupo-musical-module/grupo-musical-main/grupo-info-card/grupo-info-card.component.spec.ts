c5e0e7263a9ad9b091a2576cea1ea3669d58e46a
Daniel Reales <da.reales@uniandes.edu.co>
2022-05-12 17:22:25 -0500

    Completar las pruebas

--- src/app/grupo-musical-module/grupo-musical-main/grupo-info-card/grupo-info-card.component.spec.ts
+++ src/app/grupo-musical-module/grupo-musical-main/grupo-info-card/grupo-info-card.component.spec.ts
@@ -6,15 +6,22 @@ import { DebugElement } from '@angular/core';
 import { GrupoInfoCardComponent } from './grupo-info-card.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { GrupoMusical } from '../../grupoMusical';
+import faker from '@faker-js/faker';
+import { Proveedor } from 'src/app/proveedor/proveedor';
+import { GrupoMusicalService } from '../../GrupoMusical.service';
 
 describe('GrupoInfoCardComponent', () => {
   let component: GrupoInfoCardComponent;
   let fixture: ComponentFixture<GrupoInfoCardComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports : [HttpClientModule, RouterTestingModule],
-      declarations: [ GrupoInfoCardComponent ]
+      declarations: [ GrupoInfoCardComponent ],
+      providers: [GrupoMusicalService]
+
     })
     .compileComponents();
   }));
@@ -22,10 +29,38 @@ describe('GrupoInfoCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GrupoInfoCardComponent);
     component = fixture.componentInstance;
+
+    let proveedor = new Proveedor(
+      faker.datatype.number(),
+      faker.name.firstName(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.datatype.number().toString(),
+      faker.image.imageUrl(),
+      [],
+      [],
+      [],
+      []);
+
+    let grupoMusical = new GrupoMusical(
+      faker.datatype.number(),
+      faker.name.firstName(),
+      faker.datatype.number(),
+      faker.lorem.sentence(),
+      proveedor,
+      [],
+      [],
+      [],
+      faker.image.imageUrl());
+    component.grupoMusical_id = 1;
+    component.grupoMusical = grupoMusical;
+
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
    it('should create', () => {
-     expect(component).toBeTruthy();
+     expect(component.grupoMusical).toBeTruthy();
    });
 });
