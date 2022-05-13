67316f051a843a5741d4c22e8ef5a9709461a741
aspd-2501 <a.padillad@uniandes.edu.co>
2022-05-12 23:57:41 -0500

    release

--- src/app/zona/zona-detail/zona-detail.component.spec.ts
+++ src/app/zona/zona-detail/zona-detail.component.spec.ts
@@ -4,13 +4,21 @@
 // import { DebugElement } from '@angular/core';
 
 // import { ZonaDetailComponent } from './zona-detail.component';
+// import { faker } from '@faker-js/faker';
+// import { ZonaDetail } from '../zona-detail';
+// import { Proveedor } from 'src/app/proveedor/proveedor';
+// import { ProveedorDetail } from 'src/app/proveedor/proveedor-detail';
+// import { HttpClientModule } from '@angular/common/http';
+// import { RouterTestingModule } from '@angular/router/testing';
 
 // describe('ZonaDetailComponent', () => {
 //   let component: ZonaDetailComponent;
 //   let fixture: ComponentFixture<ZonaDetailComponent>;
+//   let debug: DebugElement;
 
 //   beforeEach(async(() => {
 //     TestBed.configureTestingModule({
+//       imports: [HttpClientModule, RouterTestingModule],
 //       declarations: [ ZonaDetailComponent ]
 //     })
 //     .compileComponents();
@@ -19,7 +27,20 @@
 //   beforeEach(() => {
 //     fixture = TestBed.createComponent(ZonaDetailComponent);
 //     component = fixture.componentInstance;
+
+//     let proveedor = new ProveedorDetail(
+//       faker.datatype.number(),  //id
+//       faker.lorem.sentence(), //nombre
+//       faker.lorem.sentence(), //descripcion
+//       faker.lorem.sentence(),  //trayectorio
+//       faker.lorem.sentence(),  //calificacion
+//       faker.image.imageUrl(), //imagen
+//       [],
+//       []
+//     );
+
 //     fixture.detectChanges();
+//     // debug = fixture.debugElement;
 //   });
 
 //   it('should create', () => {
--- src/app/zona/zona.service.spec.ts
+++ src/app/zona/zona.service.spec.ts
@@ -2,15 +2,18 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { ZonaService } from './zona.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('Service: Zona', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule],
       providers: [ZonaService]
     });
   });
 
-  // it('should ...', inject([ZonaService], (service: ZonaService) => {
-  //   expect(service).toBeTruthy();
-  // }));
+  it('should ...', inject([ZonaService], (service: ZonaService) => {
+    expect(service).toBeTruthy();
+  }));
 });
